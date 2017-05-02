1f9f7d4cfd3f1ffe7a7508d823d69d7cd1b631be
rsandell <rsandell@cloudbees.com>
2017-05-02 17:10:18 +0200

    Clean up imports

--- src/main/java/jenkins/security/plugins/ldap/LDAPConfiguration.java
+++ src/main/java/jenkins/security/plugins/ldap/LDAPConfiguration.java
@@ -1,3 +1,28 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2004-2010, Sun Microsystems, Inc., Kohsuke Kawaguchi, Seiji Sogabe,
+ *    Olivier Lamy
+ * Copyright (c) 2017 CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package jenkins.security.plugins.ldap;
 
 import groovy.lang.Binding;
@@ -6,7 +31,8 @@ import hudson.Extension;
 import hudson.Util;
 import hudson.model.AbstractDescribableImpl;
 import hudson.model.Descriptor;
-import hudson.security.*;
+import hudson.security.LDAPSecurityRealm;
+import hudson.security.SecurityRealm;
 import hudson.util.FormValidation;
 import hudson.util.Secret;
 import hudson.util.spring.BeanBuilder;
@@ -36,7 +62,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Hashtable;
@@ -92,7 +120,7 @@ public class LDAPConfiguration extends AbstractDescribableImpl<LDAPConfiguration
     private String mailAddressAttributeName;
     private Map<String,String> extraEnvVars;
     /**
-     * Set in {@link #createApplicationContext()}
+     * Set in {@link #createApplicationContext(LDAPSecurityRealm)}
      */
     private LdapTemplate ldapTemplate;
 
