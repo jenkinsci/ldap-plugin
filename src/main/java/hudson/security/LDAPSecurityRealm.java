221ab307a7a6d4775bf5c87979bb6b6c783563f9
carlosrodlop <it.carlosrodlop@gmail.com>
2019-09-26 17:22:26 +0200

    removing unused methods

--- src/main/java/hudson/security/LDAPSecurityRealm.java
+++ src/main/java/hudson/security/LDAPSecurityRealm.java
@@ -414,9 +414,6 @@ public class LDAPSecurityRealm extends AbstractPasswordBasedSecurityRealm {
 
     private boolean disableRolePrefixing;
 
-    @SuppressFBWarnings(value = "MS_SHOULD_BE_FINAL", justification = "Diagnostic fields are left mutable so that groovy console can be used to dynamically turn/off probes.")
-    //public static boolean getCnFromAttributes = Boolean.getBoolean(LDAPSecurityRealm.class.getName()+".getcnfromattributes");
-
     /**
      * @deprecated retained for backwards binary compatibility.
      */
@@ -977,27 +974,6 @@ public class LDAPSecurityRealm extends AbstractPasswordBasedSecurityRealm {
             String groupName = fixGroupname(extractGroupName(name, attributes));
             return new GroupDetailsImpl(dn, groupName);
         }
-
-/*        static String extractGroupName(LdapName name, Attributes attributes) throws NamingException {
-            final String CN = "cn";
-            String groupName = "";
-            if (!getCnFromAttributes) {
-                groupName = String.valueOf(name.getRdn(name.size() - 1).getValue());
-            } else {
-                for (NamingEnumeration ae = attributes.getAll(); ae.hasMore() && ae != null;) {
-                    Attribute attr = (Attribute) ae.next();
-                    if (CN.equals(attr.getID())) {
-                        for (NamingEnumeration e = attr.getAll(); groupName.equals("");e.hasMore()) {
-                            groupName = e.next().toString();
-                            if (e.hasMore()) {
-                                LOGGER.log(Level.FINE,"The group " + name.getRdns() + " has more than one cn value. The first one  (" + groupName + ") has been assigned as external group name");
-                            }
-                        }
-                    }
-                }
-            }
-            return groupName;
-        }*/
 
         static String extractGroupName(LdapName name, Attributes attributes) throws NamingException {
             final String CN = "cn";
