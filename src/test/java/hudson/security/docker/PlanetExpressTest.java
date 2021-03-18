377d2e14b583538f07cb2dee03e07a11a64a836c
Jesse Glick <jglick@cloudbees.com>
2021-03-18 17:51:25 -0400

    Use RealJenkinsRule in PlanetExpressTest

--- src/test/java/hudson/security/docker/PlanetExpressTest.java
+++ src/test/java/hudson/security/docker/PlanetExpressTest.java
@@ -13,6 +13,7 @@ import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.RealJenkinsRule;
 import org.springframework.security.ldap.userdetails.LdapUserDetails;
 import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.GenericContainer;
@@ -40,23 +41,32 @@ public class PlanetExpressTest {
     public GenericContainer container = new GenericContainer(TEST_IMAGE).withExposedPorts(389);
 
     @Rule
-    public JenkinsRule j = new JenkinsRule();
+    public RealJenkinsRule rr = new RealJenkinsRule();
 
     @Test
-    public void login() throws Exception {
-        LDAPSecurityRealm realm = new LDAPSecurityRealm(container.getHost() + ":" + container.getFirstMappedPort(), DN, null, null, null, null, null, MANAGER_DN, Secret.fromString(MANAGER_SECRET), false, false, null, null, "cn", "mail", null,null);
-        j.jenkins.setSecurityRealm(realm);
-        j.configRoundtrip();
-        String content = j.createWebClient().login("fry", "fry").goTo("whoAmI").getBody().getTextContent();
-        assertThat(content, containsString("Philip J. Fry"));
+    public void login() throws Throwable {
+        String server = container.getHost() + ":" + container.getFirstMappedPort();
+        rr.then(new Login(server));
+    }
+    private static class Login implements RealJenkinsRule.Step {
+        private final String server;
+        Login(String server) {
+            this.server = server;
+        }
+        @Override
+        public void run(JenkinsRule j) throws Throwable {
+            LDAPSecurityRealm realm = new LDAPSecurityRealm(server, DN, null, null, null, null, null, MANAGER_DN, Secret.fromString(MANAGER_SECRET), false, false, null, null, "cn", "mail", null,null);
+            j.jenkins.setSecurityRealm(realm);
+            j.configRoundtrip();
+            String content = j.createWebClient().login("fry", "fry").goTo("whoAmI").getBody().getTextContent();
+            assertThat(content, containsString("Philip J. Fry"));
 
+            LdapUserDetails zoidberg = (LdapUserDetails) j.jenkins.getSecurityRealm().loadUserByUsername2("zoidberg");
+            assertEquals("cn=John A. Zoidberg,ou=people,dc=planetexpress,dc=com", zoidberg.getDn());
 
-        LdapUserDetails zoidberg = (LdapUserDetails) j.jenkins.getSecurityRealm().loadUserByUsername2("zoidberg");
-        assertEquals("cn=John A. Zoidberg,ou=people,dc=planetexpress,dc=com", zoidberg.getDn());
-
-        String leelaEmail = MailAddressResolver.resolve(j.jenkins.getUser("leela"));
-        assertEquals("leela@planetexpress.com", leelaEmail);
-
+            String leelaEmail = MailAddressResolver.resolve(j.jenkins.getUser("leela"));
+            assertEquals("leela@planetexpress.com", leelaEmail);
+        }
     }
 
 }
