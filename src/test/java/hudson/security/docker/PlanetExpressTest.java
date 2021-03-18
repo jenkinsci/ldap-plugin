786de55ff6856392c0ef295929a0ca35606bbd3f
Jesse Glick <jglick@cloudbees.com>
2021-03-18 17:45:18 -0400

    docker-fixtures → testcontainers

--- src/test/java/hudson/security/docker/PlanetExpressTest.java
+++ src/test/java/hudson/security/docker/PlanetExpressTest.java
@@ -4,38 +4,47 @@ import hudson.Functions;
 import hudson.security.LDAPSecurityRealm;
 import hudson.tasks.MailAddressResolver;
 import hudson.util.Secret;
-import org.springframework.security.ldap.userdetails.LdapUserDetails;
-import org.jenkinsci.test.acceptance.docker.DockerContainer;
-import org.jenkinsci.test.acceptance.docker.DockerFixture;
-import org.jenkinsci.test.acceptance.docker.DockerRule;
-import org.junit.Rule;
-import org.junit.Test;
-import org.jvnet.hudson.test.JenkinsRule;
-
 import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeTrue;
 import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.springframework.security.ldap.userdetails.LdapUserDetails;
+import org.testcontainers.DockerClientFactory;
+import org.testcontainers.containers.GenericContainer;
 
 /**
  * Tests the plugin when logging in to rroemhild/test-openldap
  */
 public class PlanetExpressTest {
 
+    static final String TEST_IMAGE = "rroemhild/test-openldap@sha256:b4e433bbcba1f17899d6bcb0a8e854bbe52c754faa4e785d0c27a2b55eb12cd8";
+    static final String DN = "dc=planetexpress,dc=com";
+    static final String MANAGER_DN = "cn=admin,dc=planetexpress,dc=com";
+    static final String MANAGER_SECRET = "GoodNewsEveryone";
+
+    @BeforeClass public static void requiresDocker() {
+        assumeTrue(DockerClientFactory.instance().isDockerAvailable());
+    }
+
     @BeforeClass public static void linuxOnly() {
         assumeFalse("Windows CI builders now have Docker installed…but it does not support Linux images", Functions.isWindows() && System.getenv("JENKINS_URL") != null);
     }
 
+    @SuppressWarnings("rawtypes")
     @Rule
-    public DockerRule<PlanetExpress> docker = new DockerRule<>(PlanetExpress.class);
+    public GenericContainer container = new GenericContainer(TEST_IMAGE).withExposedPorts(389);
+
     @Rule
     public JenkinsRule j = new JenkinsRule();
 
     @Test
     public void login() throws Exception {
-        PlanetExpress d = docker.get();
-        LDAPSecurityRealm realm = new LDAPSecurityRealm(d.ipBound(389) + ":" + d.port(389), PlanetExpress.DN, null, null, null, null, null, PlanetExpress.MANAGER_DN, Secret.fromString(PlanetExpress.MANAGER_SECRET), false, false, null, null, "cn", "mail", null,null);
+        LDAPSecurityRealm realm = new LDAPSecurityRealm(container.getHost() + ":" + container.getFirstMappedPort(), DN, null, null, null, null, null, MANAGER_DN, Secret.fromString(MANAGER_SECRET), false, false, null, null, "cn", "mail", null,null);
         j.jenkins.setSecurityRealm(realm);
         j.configRoundtrip();
         String content = j.createWebClient().login("fry", "fry").goTo("whoAmI").getBody().getTextContent();
@@ -50,12 +59,4 @@ public class PlanetExpressTest {
 
     }
 
-    @DockerFixture(id = "openldap-express", ports = {389, 636})
-    public static class PlanetExpress extends DockerContainer {
-
-        static final String DN = "dc=planetexpress,dc=com";
-        static final String MANAGER_DN = "cn=admin,dc=planetexpress,dc=com";
-        static final String MANAGER_SECRET = "GoodNewsEveryone";
-
-    }
 }
