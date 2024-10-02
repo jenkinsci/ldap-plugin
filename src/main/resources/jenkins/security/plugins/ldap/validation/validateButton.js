2f20b801f120a3cf635b9aada3e01da0c04fa855
Zbynek Konecny <zbynek1729@gmail.com>
2024-10-02 04:58:04 +0200

    Use new dialog design for validation dialog (#277)

--- src/main/resources/jenkins/security/plugins/ldap/validation/validateButton.js
+++ src/main/resources/jenkins/security/plugins/ldap/validation/validateButton.js
@@ -1,11 +1,8 @@
 
  Behaviour.specify(".ldap-validate-button-reference-holder", 'ldap-validate', 0, function (e) {
-     var url = e.getAttribute('data-fullurl');
-     var attr = e.getAttribute('data-attributes')
-     var id = e.getAttribute('data-id');
-     var button = document.getElementById(id);
+     const button = document.getElementById(e.dataset['id']);
      button.onclick = function(el) {
-        ldapValidateButton(url,attr,this,id);
+        ldapValidateButton(this, e.dataset);
         return false;
      }
  });
